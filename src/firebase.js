23ad85ca662bdbda07404af5873e59e07cccb8fd
Ronen Fromchuk <90146636+ronenfromchuk@users.noreply.github.com>
2022-04-30 14:52:19 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB5V66BEVL55O98uJVW2crV17R4cTQxd8c",
-    authDomain: "linkedin-clone-7adf3.firebaseapp.com",
-    projectId: "linkedin-clone-7adf3",
-    storageBucket: "linkedin-clone-7adf3.appspot.com",
-    messagingSenderId: "836256101657",
-    appId: "1:836256101657:web:10a29beed16988e1b40188"
+    apiKey: "*****************************************",
+    authDomain: "*****************************************",
+    projectId: "*****************************************",
+    storageBucket: "*****************************************",
+    messagingSenderId: "*****************************************",
+    appId: "*****************************************",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
